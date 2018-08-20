4bc61c7bcb79db425a57fe85f9493b8767391312
Ginwene Rueda <gmrueda@mmm.com>
2018-08-20 02:36:42 +0000

    Added week 1 source code.

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -3,57 +3,43 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("bootcamptodolist.controller.Home", {
+	return Controller.extend("bootcamptodolist.controller.Details", {
+
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf bootcamptodolist.view.Home
+		 * @memberOf bootcamptodolist.view.Details
 		 */
-		onInit: function() {
-			alert("Hello from init");
-		},
+		//	onInit: function() {
+		//
+		//	},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf bootcamptodolist.view.Home
+		 * @memberOf bootcamptodolist.view.Details
 		 */
-		onBeforeRendering: function() {
-
-		},
+		//	onBeforeRendering: function() {
+		//
+		//	},
 
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf bootcamptodolist.view.Home
+		 * @memberOf bootcamptodolist.view.Details
 		 */
-		onAfterRendering: function() {
-
-		},
+		//	onAfterRendering: function() {
+		//
+		//	},
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf bootcamptodolist.view.Home
+		 * @memberOf bootcamptodolist.view.Details
 		 */
-		onExit: function() {
+		//	onExit: function() {
+		//
+		//	}
 
-		},
-
-		/**
-		 * Event handler for button
-		 * @memberOf bootcamptodolist.view.Home
-		 */
-		onPressButton: function(oEvent) {
-			alert("Hello from button");
-			this._myPrivateFunction();
-		},
-
-		/**
-		 * Function called from onPressButton
-		 * @memberOf bootcamptodolist.view.Home
-		 */
-		_myPrivateFunction: function() {
-			alert("Hello from private function.");
-		}
 	});
+
 });
